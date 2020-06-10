568a609296dfd471d34ffab4af7a87265dec9e74
hsngrms <1662498+hsngrms@users.noreply.github.com>
2020-06-10 12:04:45 +0300

    Add files via upload

---
+++ SubModule.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace Enable_FaceGen_Sliders
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            new Harmony("hsngrms.enable.facegen.sliders").PatchAll();
+            try
+            {
+                TaleWorlds.Core.FaceGen.ShowDebugValues = true;
+            }
+            catch
+            {
+            }
+        }
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+            InformationManager.DisplayMessage(new InformationMessage("FaceGen (Body) Sliders enabled"));
+        }
+    }
+}
